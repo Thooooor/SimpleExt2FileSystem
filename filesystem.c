f2108fc55fb149c77cfd5702ca17f550ee4e5b19
Thooooor <821403039@qq.com>
2020-12-10 11:19:56 +0800

    Create FileSystem

---
+++ filesystem.c
@@ -0,0 +1,52 @@
+#include "filesystem.h"
+#include "disk.h"
+#include "superblock.h"
+#include <stdio.h>
+
+sp_block *spb;
+
+int init_system() {
+    init_sp_block(spb);
+    
+    if (!write_sp_block(spb)) {
+        printf("Initial super bolck failed.\n");
+        return -1;
+    }
+    printf("Initial super bolck succeded.Enjoy your Burger.\n");
+    return 1;
+}
+
+int open_system() {
+    if (!open_disk()) {
+        printf("Open disk error.\n");
+        return -1;
+    }
+    if (read_sp_block(spb) && spb->magic_num == MAGICNUM) {
+        printf("Enjoy your Burger.\n");
+    } else {
+        printf("File System Unkonwn or didn't exist. Format disk and build a burger file system?(y/n)\n");
+        char option;
+        scanf("%c", &option);
+        if (option == 'y') {
+            init_system();
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
+
+int close_system() {
+    if (!write_sp_block(spb)) {
+        printf("Save super bolck failed.\n");
+        return -1;
+    }
+
+    if (close_disk() < 0) {
+        printf("Close disk failed.\n");
+    }
+
+    printf("Hoped you enjoyed your burger. See you.\n");
+    return 1;
+    
+}
--- filesystem.h
+++ filesystem.h
@@ -1,3 +1,6 @@
+#define MAGICNUM 0xABCD
+#define INODENUM 32
+#define BLOCKNUM 128
 
 /**
  * 超级块
@@ -8,8 +11,8 @@ typedef struct super_block
     int free_block_count;
     int free_inode_count;
     int dir_inode_count;
-    unsigned int block_map[128];
-    unsigned int inode_map[32];
+    unsigned int block_map[BLOCKNUM];
+    unsigned int inode_map[INODENUM];
 } sp_block;
 
 
@@ -29,4 +32,44 @@ struct dir_item
     char name[121];
 };
 
+/**
+ * @brief initial Ext2 file system
+ * 
+ * @return returns 0 on success, -1 otherwise. 
+ * 
+ * @note This function will initial Ext2 file system named "burger" as a vritual file system
+ * If the file system is not found, it will try to create the file, and fill it with zeros of 4 MiB.
+ * This function must be called before any calls to disk_read_block() and disk_write_block().
+ * This function will fail if the disk is already opened.
+ */
+int init_system();
 
+/**
+ * @brief Open the Ext2 file system.
+ * 
+ * @return returns 0 on success, -1 otherwise. 
+ * 
+ * @note This function will open a file system named "burger" as a vritual file system
+ * If the file is not found, it will try to create the file, and fill it with zeros of 4 MiB.
+ * This function must be called before any calls to disk_read_block() and disk_write_block().
+ * This function will fail if the disk is already opened.
+ */
+int open_system();
+
+/**
+ * @brief Close the Ext2 file system. 
+ * 
+ * @return returns 0 on success, -1 otherwise. 
+ * 
+ * @note This function will open a file named "disk" as a vritual disk
+ * If the file is not found, it will try to create the file, and fill it with zeros of 4 MiB.
+ * This function must be called before any calls to disk_read_block() and disk_write_block().
+ * This function will fail if the disk is already opened.
+ */
+int close_system();
+
+int make_dir();
+
+int make_file();
+
+int copy_file();
